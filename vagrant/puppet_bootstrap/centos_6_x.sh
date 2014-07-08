fcdf09f633bf366066537f041b6a0c67281b5a9c
Adam Buxton <buxton.adam@gmail.com>
2014-07-08 14:12:56 +0100

    added place holder for alt version of ruby

--- vagrant/puppet_bootstrap/centos_6_x.sh
+++ vagrant/puppet_bootstrap/centos_6_x.sh
@@ -14,6 +14,7 @@ yum install bzip2 autoconf automake libtool bison iconv-devel -y
 curl -L get.rvm.io | bash -s stable
 source /etc/profile.d/rvm.sh
 #install a newer ruby version
+#rvm install 1.8.7
 rvm install 1.9.3
 
 #lets create some repos and variables
