496e55431fc7256490987fe252ed3f978847c773
Adam Buxton <buxton.adam@gmail.com>
2014-07-08 13:03:22 +0100

    update boot script for sanity

--- vagrant/puppet_bootstrap/centos_6_x.sh
+++ vagrant/puppet_bootstrap/centos_6_x.sh
@@ -2,31 +2,37 @@
 # This bootstraps Puppet on CentOS 6.x
 # It has been tested on CentOS 6.3 64bit
 # forked from https://github.com/hashicorp/puppet-bootstrap/blob/master/centos_6_x.sh
-set -e
-
-REPO_URL="http://yum.puppetlabs.com/el/6/products/i386/puppetlabs-release-6-6.noarch.rpm"
+set -ex
+REPO='puppetlabs-release-6-6.noarch' 
+REPO_URL="http://yum.puppetlabs.com/el/6/products/i386/${REPO}.rpm"
 
 if [ "$EUID" -ne "0" ]; then
   echo "This script must be run as root." >&2
   exit 1
 fi
 
-if which puppet > /dev/null 2>&1; then
-  echo "Puppet is already installed"
-  exit 0
-fi
 
 # Install puppet labs repo
-echo "Configuring PuppetLabs repo..."
-repo_path=$(mktemp)
-wget --output-document=${repo_path} ${REPO_URL} 2>/dev/null
-rpm -i ${repo_path} >/dev/null
+if ! [[ $( rpm -qa $REPO ) =~ ${REPO} ]]
+then 
+  echo "Configuring PuppetLabs repo..."
+  repo_path=$(mktemp)
+  wget --output-document=${repo_path} ${REPO_URL} 2>/dev/null
+  rpm -i ${repo_path} >/dev/null 2>&1
 
-# Install Puppet...
-echo "Installing puppet"
-yum install -y puppet > /dev/null
+else
+  echo "${REPO} is already installed"
+fi
 
-echo "Puppet installed!"
+if which puppet > /dev/null 2>&1; then
+  echo "Puppet is already installed"
+  #exit 0
+else
+  # Install Puppet...
+  echo "Installing puppet"
+  yum install -y puppet > /dev/null
+  echo "Puppet installed!"
+fi
 
 echo "Install tree, rass thrassing grr"
 yum install -y tree > /dev/null
